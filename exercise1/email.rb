58667bcb0090c2adf04e66cafd5e1ecbb8f8f456
zem3 <zemm10a@gmail.com>
2015-02-24 19:18:55 +0000

    done

---
+++ exercise1/email.rb
@@ -0,0 +1,19 @@
+class Email
+  
+  attr_reader :subject
+  attr_reader :date
+  attr_reader :from
+  
+  def initialize(subject, date, from)
+  	@subject=subject
+  	@date=date
+  	@from=from
+  end
+
+end
+
+email = Email.new("Homework this week", "2014-12-01", "Ferdous")
+
+puts "Date:    #{email.date}"
+puts "From:    #{email.from}"
+puts "Subject: #{email.subject}"
