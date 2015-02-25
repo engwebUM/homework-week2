60c4c0563e7fd663b1b2354d94cfed056494fe6c
jmtalves <alvesjose13@gmail.com>
2015-02-25 20:47:23 +0000

    Exercise 3 version 4

--- exercise3/mailbox.rb
+++ exercise3/mailbox.rb
@@ -12,6 +12,7 @@ end
 
 class Mailbox
   attr_reader :emails
+  attr_reader :name
     def initialize(name,emails)
       @name=name
       @emails=emails
@@ -27,13 +28,71 @@ class MailboxTextFormatter
     end
     
     def format
-      rows1 = []
+      puts "#{@mailbox.name}"
+      puts ""
+      @xtot=0 # total size
+      @xfrom=0 #size to from
       @mailbox.emails.each do |email|
-      rows1 << [email.date, email.from, email.subject]
+        @size=email.subject.size
+        @sizefrom=email.from.size
+        if @xtot<@size 
+          @xtot=@size
+        end
+        if @xfrom<@sizefrom
+          @xfrom=@sizefrom
+        end
       end
-      table = Terminal::Table.new :headings => ['Date', 'From', 'Subject'], :rows => rows1
-      puts table
+      @xtot+=@xfrom+20 #20 - size characters Date and spaces
+      @xfrom+=16    #16 - size characters Date and spaces to 'from'
+      separator
+      printHeader
+      separator
+      printRows(@mailbox)
+      separator
+      return ""
     end
+
+    def printHeader
+      print "| Date"
+      loop(6,12," ")
+      print "| From"
+      loop(20,@xfrom," ")
+      print "| Subject"
+      loop(@xfrom+11,@xtot," ")
+      print "|"
+      puts ""
+    end
+
+    def printRows (mailbox)
+      @mailbox.emails.each do |email|
+        print "| #{email.date}"
+        loop(1,1," ")
+        print "| #{email.from}"
+        loop(15,@xfrom-email.from.size-1," ")
+        print "| #{email.subject}"
+        loop(@xfrom,@xtot-email.subject.size-4," ")
+        print "|"
+        puts ""
+      end
+    end 
+
+    def separator
+      print "+"
+      loop(1,12,"-")
+      print "+"
+      loop(15,@xfrom,"-")
+      print "+"
+      loop(@xfrom,@xtot-3,"-")
+      print "+" 
+      puts ""
+    end 
+
+    def loop (p_start, p_end, c_Print)
+      for i in p_start..p_end
+        print c_Print
+      end
+    end 
+
 end
 
 emails = [
