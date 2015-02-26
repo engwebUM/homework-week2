e2f518ad0a2585c4efe6c66089cdbed71a6a11d8
luissilva28 <pg24165@alunos.uminho.pt>
2015-02-26 21:38:31 +0000

    Exercise1.1 and 1.2 Solved
    Exercise1.1 and 1.2 Solved

---
+++ exercise1/email.rb
@@ -0,0 +1,16 @@
+class Email
+	# fill in this class body
+	attr_reader :subject, :date, :from
+
+	def initialize(subject, date, from)
+		@subject = subject
+		@date = date
+		@from = from
+	end
+end
+
+email = Email.new("Homework this week", "2014-12-01", "Ferdous")
+
+puts "Date: #{email.date}"
+puts "From: #{email.from}"
+puts "Subject: #{email.subject}"
