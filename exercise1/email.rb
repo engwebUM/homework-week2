78b3d9ede45197ab956495397bed04af5fc3729d
sandroferraz14 <sandro.ferraz.mei@gmail.com>
2015-02-27 01:33:50 +0000

    resolution of exercise 1.1

--- exercise1/email.rb
+++ exercise1/email.rb
@@ -1,3 +1,25 @@
 class Email
- 
+	def initialize(subject, date, from)
+      @subject = subject
+      @date = date
+      @from = from
+   end
+
+   def subject
+   	  @subject
+   end
+
+   def date
+   	  @date
+   end
+   
+   def from
+   	  @from
+   end
 end
+
+email = Email.new("of your interest", "2014-12-12", "A guy")
+
+puts "Date:    #{email.date}"
+puts "From:    #{email.from}"
+puts "Subject: #{email.subject}"
