69bb466ee2cde1dce9d842c3c032b312685bc041
DuarteDuarte <dd3m@hotmail.com>
2015-03-01 05:13:42 +0000

    Ex-3 - Exercicios resolvido.

--- exercise3/mailbox.rb
+++ exercise3/mailbox.rb
@@ -56,13 +56,15 @@ class MailboxTextFormatter
 		# basta saber o tamanho máximo dos campos from e subject
 		# max têm no minimo o tamanho dos nomes das colunas
 		
-		max_from = 20
-		max_subject = 25
+		max_from = def_max_from (mailbox)
+		max_subject = def_max_subject (mailbox)
 		espaco = ' '
 		hifen = '-'
 		
+		puts
+		puts
 		puts "Mailbox: #{mailbox.name}"	
-		puts "+#{hifen*12}+-#{hifen * max_from}-+-#{hifen * max_subject}-+"
+		puts "+#{hifen * 12}+-#{hifen * max_from}-+-#{hifen * max_subject}-+"
 		puts "| Data #{espaco*6}| From#{espaco*(max_from - 4)} | Subject#{espaco*(max_subject - 7)} |"
 		puts "+#{hifen*12}+-#{hifen * max_from}-+-#{hifen * max_subject}-+"
 		mailbox.emails.each do |email|	
@@ -71,12 +73,28 @@ class MailboxTextFormatter
 		puts "+#{hifen*12}+-#{hifen * max_from}-+-#{hifen * max_subject}-+"
 	end
 	
-	def def_max_from
-	
+	def def_max_from (mailbox)
+		max = 4
+		mailbox.emails.each do |email|	
+			if email.from.length > max	
+				max = email.from.length
+			else
+				max
+			end
+		end
+		return max
 	end
 	
-	def def_max_subject
-	
+	def def_max_subject (mailbox)
+		max = 7
+		mailbox.emails.each do |email|	
+			if email.subject.length > max	
+				max = email.subject.length
+			else
+				max
+			end
+		end
+		return max
 	end
 	
 	
@@ -92,27 +110,3 @@ mailbox = Mailbox.new("Ruby Study Group", emails)
 formatter = MailboxTextFormatter.new(mailbox)
 
 puts formatter.format
-
-# Your goal is to complete the code in a way so it outputs the following:
-
-# Mailbox: Ruby Study Group
-
-# +------------+---------+------------------------+
-# | Date       | From    | Subject                |
-# +------------+---------+------------------------+
-# | 2014-12-01 | Ferdous | Homework this week     |
-# | 2014-12-01 | Dajana  | Keep on coding! :)     |
-# | 2014-12-02 | Ariane  | Re: Homework this week |
-# +------------+---------+------------------------+
-# You are allowed to add as many methods to the classes Email, Mailbox and 
-# MailboxTextFormatter as you deem useful. In your final solution you are 
-# not allowed to change any of the code outside (after) the class definitions. 
-# For debugging purposes, you can, of course, change all the code you want.
-
-# Try to come up with a working solution first. It does not matter how elegant, 
-# generic, or pretty it is. Whatever produces the required output is fine for a 
-# first solution. Then, later, look at your code, and try to improve it by cleaning 
-# up everything that you do not like, or deem ugly.
-
-# Eventually, one goal to aim for would be: Adding another column to the table only 
-# requires minimal changes, e.g. changes to one or two places in your formatter class.
