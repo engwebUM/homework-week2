be520744dfb747187ec3977c5eeee456fc146f79
sandroferraz14 <sandro.ferraz.mei@gmail.com>
2015-03-01 19:39:17 +0000

    third version

--- exercise3/mailbox.rb
+++ exercise3/mailbox.rb
@@ -1,5 +1,5 @@
 class Email
-    attr_accessor :subject, :date, :from
+    attr_reader :subject, :date, :from
 
 	def initialize(subject, options={})
    		@subject, @date, @from  = subject, options[:date], options[:from]
@@ -7,7 +7,7 @@ class Email
 end
 
 class Mailbox
-	attr_accessor :name
+	attr_reader :name
 
 	def initialize(name, emails)
 		@name, @emails = name, emails 
@@ -19,15 +19,17 @@ class Mailbox
 end
 
 class MailboxTextFormatter
+	attr_reader :mailbox
+
 	def initialize(mailbox)
 		@mailbox = mailbox
 	end
 
 	def getDimensionsTable(mailbox)
 		dimensionsTable = {}
-		dateRow = 0
-		subjectRow = 0
-		fromRow = 0
+		dateRow = 4
+		subjectRow = 4
+		fromRow = 7
 		@mailbox.emails.map do |email| 
 			if email.date.size > dateRow
 	 			dateRow = email.date.size
@@ -43,21 +45,25 @@ class MailboxTextFormatter
 	end
 
 	def format
-		stringT = ""
+		content = []
 		dimensionsColumns = getDimensionsTable(@mailbox)
 		dateRowDimension = dimensionsColumns[0]
 		fromRowDimension = dimensionsColumns[2]
 		subjectRowDimension = dimensionsColumns[1]
-		totalWidthTable = dateRowDimension + fromRowDimension + subjectRowDimension + 10
+		totalWidthTable = dateRowDimension + fromRowDimension + subjectRowDimension + 8
 		formatCells = '| %-' + dateRowDimension.to_s + 's | %-' + fromRowDimension.to_s + 's | %-' + subjectRowDimension.to_s + 's |'
-		puts totalWidthTable.times.collect {'-'}.join()
-		puts formatCells % ['Date', 'From', 'Subject']
-		puts totalWidthTable.times.collect {'-'}.join()
+		formatSeparator = formatCells.gsub('|', '+')
+		separator = formatSeparator % ['-' * dateRowDimension, '-' * fromRowDimension, '-' * subjectRowDimension]
+		content << 'Mailbox: ' + @mailbox.name
+		content << ''
+		content << separator
+		content << formatCells % ['Date', 'From', 'Subject']
+		content << separator
 		@mailbox.emails.map do |email| 
-			puts formatCells % [email.date, email.from, email.subject]
+			content << formatCells % [email.date, email.from, email.subject]
 		end
-		puts totalWidthTable.times.collect {'-'}.join()
-
+		content << separator
+		content << ''
 	end
 end
 
@@ -69,5 +75,10 @@ emails = [
 
 mailbox = Mailbox.new("Ruby Study Group", emails)
 formatter = MailboxTextFormatter.new(mailbox)
+
+puts formatter.format
+
+addNewEmail = Email.new("Add a new email for test, bla, bla, bla", { :date => "2015-03-01", :from => "Sandro" })
+formatter.mailbox.emails << addNewEmail
 
 puts formatter.format
