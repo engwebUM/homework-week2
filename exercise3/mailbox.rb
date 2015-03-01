1e28b7031cc3994ac05c8eff0a2bff043707f91c
sandroferraz14 <sandro.ferraz.mei@gmail.com>
2015-03-01 18:38:11 +0000

    second version

--- exercise3/mailbox.rb
+++ exercise3/mailbox.rb
@@ -23,12 +23,40 @@ class MailboxTextFormatter
 		@mailbox = mailbox
 	end
 
-	def format
-		formatCells = '%-15s %-15s %-20s'
-		puts formatCells % ['Date', 'From', 'Subject']
+	def getDimensionsTable(mailbox)
+		dimensionsTable = {}
+		dateRow = 0
+		subjectRow = 0
+		fromRow = 0
 		@mailbox.emails.map do |email| 
-			formatCells % [email.date, email.from, email.subject]
+			if email.date.size > dateRow
+	 			dateRow = email.date.size
+	 		end
+	 		if email.subject.size > subjectRow
+	 			subjectRow = email.subject.size
+	 		end
+	 		if email.from.size > fromRow
+	 			fromRow = email.from.size
+	 		end
 		end
+		@dimensionsTable = [dateRow, subjectRow, fromRow]
+	end
+
+	def format
+		stringT = ""
+		dimensionsColumns = getDimensionsTable(@mailbox)
+		dateRowDimension = dimensionsColumns[0]
+		fromRowDimension = dimensionsColumns[2]
+		subjectRowDimension = dimensionsColumns[1]
+		totalWidthTable = dateRowDimension + fromRowDimension + subjectRowDimension + 10
+		formatCells = '| %-' + dateRowDimension.to_s + 's | %-' + fromRowDimension.to_s + 's | %-' + subjectRowDimension.to_s + 's |'
+		puts totalWidthTable.times.collect {'-'}.join()
+		puts formatCells % ['Date', 'From', 'Subject']
+		puts totalWidthTable.times.collect {'-'}.join()
+		@mailbox.emails.map do |email| 
+			puts formatCells % [email.date, email.from, email.subject]
+		end
+		puts totalWidthTable.times.collect {'-'}.join()
 
 	end
 end
