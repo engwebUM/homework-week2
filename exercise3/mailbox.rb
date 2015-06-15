d3d068d71babe50b4cb68654fc83838e64204e6f
Carlos Castro <carlos@mobiware.pt>
2015-06-15 02:18:23 +0100

    Added feature to smaller titles and code refactor

--- exercise3/mailbox.rb
+++ exercise3/mailbox.rb
@@ -19,53 +19,69 @@ class MailboxTextFormatter
 	
 	def initialize(mailbox)
 		@mailbox = mailbox
-
 	end
 
-	def format()
-		@from_offset = ""
-		@subject_offset = ""
-		@from = ""
-		@subject = ""
+  def dateSize
+    minSize = 0
+    for email in @mailbox.emails
+      if email.date.length > minSize
+        minSize=email.date.length
+      end
+    end
+    return minSize
+  end
+  
+  def fromSize
+    minSize = 0
+    for email in @mailbox.emails
+      if email.from.length > minSize
+        minSize=email.from.length
+      end
+    end
+    return minSize
+  end
+  
+  def subjectSize
+    minSize = 0
+    for email in @mailbox.emails
+      if email.subject.length > minSize
+        minSize=email.subject.length
+      end
+    end
+    return minSize
+  end
+  
+  
+	def format
+    dateLength = dateSize
+    fromLength = fromSize
+    subjectLength = subjectSize
+		
+		
 		
 		puts "Mailbox: "+mailbox.name
 		puts
 		
-		puts "+------------+---------+------------------------+"
-		puts "| Date       | From    | Subject                |"
-		puts "+------------+---------+------------------------+"
-		mailbox.emails.each do |email|
-			
-			@from = email.from
-			@subject = email.subject
-			
-			if email.from.length < 9
-				@from_offset = 7 - email.from.length
-			else
-				@from_offset = 0
-				@from = @from[0, @from.length - (@from.length-4)]+"..."
-			end
-			
-			
-			if email.subject.length < 24
-				@subject_offset = 22 - email.subject.length
-			else
-				@subject_offset = 0
-				@subject = @subject[0, @subject.length - (@subject.length-19)]+"..."
-			end
-			
-			puts "| "+email.date+" | "+@from+" "*@from_offset+" | "+@subject+" "*@subject_offset+" |"
+    puts "+-"+"-"*dateLength+"-+-"+"-"*fromLength+"-+-"+"-"*subjectLength+"-+"
+    puts "| "+"Date"+" "*(dateLength-4)+" | "+"From"+" "*(fromLength-4)+" | "+"Subject"+" "*(subjectLength-7)+" |"
+		puts "+-"+"-"*dateLength+"-+-"+"-"*fromLength+"-+-"+"-"*subjectLength+"-+"
+		
+    
+    
+    mailbox.emails.each do |email|
+      puts "| "+email.date+" "*(dateLength-email.date.length)+" | "+email.from+" "*(fromLength-email.from.length)+" | "+email.subject+" "*(subjectLength-email.subject.length)+" |"
 		end
-		puts "+------------+---------+------------------------+"
+		puts "+-"+"-"*dateLength+"-+-"+"-"*fromLength+"-+-"+"-"*subjectLength+"-+"
 	end
-	attr_reader :mailbox, :from
+	
+  attr_reader :mailbox, :from, :subject
 end
 
 
 emails = [
-	Email.new("Homework this week", { :date => "2014-12-01", :from => "Ferdous" }),
+  Email.new("Re: Homework this week", { :date => "2014-12-01", :from => "Ferdous" }),
 	Email.new("Keep on coding! :)", { :date => "2014-12-01", :from => "Dajana" }),
-	Email.new("Re: Homework this week", { :date => "2014-12-02", :from => "Ariane" })
+	Email.new("Re: Homework", { :date => "2014-12-02", :from => "Ariane" })
 ]
 mailbox = Mailbox.new("Ruby Study Group", emails)
 formatter = MailboxTextFormatter.new(mailbox)
